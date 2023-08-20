08f8ea87b365c2da551d3515c41e0df7346c2820
yoseph0524 <yoseph0524@g.ucla.edu>
2023-08-20 13:23:00 -0700

    adding css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 function Detail() {
   const { id } = useParams();
   const getMovie = async () => {
